c2b39b4a2e2d790efdbc9c100a973d785a0bf841
arushan2 <rockarush2@gmail.com>
2024-07-06 19:45:17 +0530

    personal

--- src/App.js
+++ src/App.js
@@ -3,9 +3,22 @@ import './App.css';
 function Heading(){
   return(<h1 id="Heading">Bio data</h1>)
 }
+function PersonalDetails(){
+  return(
+    <div id="Personal">
+      <h3>Personal Details</h3>
+      <p>Name:- Sujitharan Arushan</p>
+      <p>Age:- 21</p>
+      <p>Address:- Navaly east Manipay</p>
+    </div>
+  )
+}
 function App() {
   return(
+    <div>
     <Heading />
+    <PersonalDetails />
+    </div>
   )
 }
 
